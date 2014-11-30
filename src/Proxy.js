603a42132eb6bad5e6f1d741e5941144d6b3d705
psieniarski <paabson@gmail.com>
2014-11-30 15:19:10 +0100

    Automated commit

--- src/Proxy.js
+++ src/Proxy.js
@@ -1,10 +1,13 @@
 'use strict';
 
 module.exports = {
-	Request: function() {
-		this.prepare = function() {
+	Request: function(obj) {
+		var data; 
 
-		},
+		if (obj.type == 'movies-list') {
+			obj.data
+		} 
+
 
 		this.execute = function() {
 
