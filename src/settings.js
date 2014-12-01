e6209badddededa2ad5eda527c09ed5b5dc79a8e
psieniarski <paabson@gmail.com>
2014-12-01 19:04:42 +0100

    Automated commit

--- src/settings.js
+++ src/settings.js
@@ -1,7 +1,7 @@
 module.exports = {
 	urls: {
 		data: 'https://ssl.filmweb.pl/api?',
-		search: 'http:///www.filmweb.pl/search/live',	
+		search: 'http:///www.filmweb.pl/search/live/',	
 	},
 		
 	apiKey: 	'qjcGhW2JnvGT9dfCt3uT_jozR3s', 
