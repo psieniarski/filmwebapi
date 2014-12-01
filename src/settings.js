e35d363bc8e068e8365b749e0ec15d6708e99e58
psieniarski <paabson@gmail.com>
2014-12-01 17:51:44 +0100

    Automated commit

--- src/settings.js
+++ src/settings.js
@@ -7,6 +7,6 @@ module.exports = {
 	apiKey: 	'qjcGhW2JnvGT9dfCt3uT_jozR3s', 
 	appId: 		'android', 
 	version: 	'1.0',
-	log: 		console.log.apply(console), 
+	log: 		console.log.call(console), 
 
 }
