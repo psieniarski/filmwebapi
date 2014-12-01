edbd9ac5b219367de15e7f2ab23e64d438cfc7ff
psieniarski <paabson@gmail.com>
2014-12-01 17:36:01 +0100

    Automated commit

--- src/net.js
+++ src/net.js
@@ -1,20 +1,27 @@
 
-function Net() {
+function XHR() {
 	var xhr = new XMLHttpRequest();
 	
-	xhr.onreadystatechange = function() {
-	    if (xhr.readyState == 4) {
-	       console.log(JSON.stringify(xhr.responseText));
-	    }
-	}
-
-	this.request = function() {
-		xhr.open('GET',settings.urls.api + serialize(data), true); 
+	this.IDRequest = function(parms) {
+		xhr.open('GET',settings.urls.api + parms, true); 
 		xhr.send();
 	},
 
-	this.response = function() {
+	this.dataRequest = function(parms) {
+		xhr.open('GET',settings.urls.api + parms, true);
+		xhr.send();
+	},
 
+	this.response = function(callback) {
+		xhr.onreadystatechange = function() {
+		    if (xhr.readyState == 4) {
+		       callback(xhr.responseText);
+		    }
+		}
 	}
-
 }
+
+XHR.response(function(a) {
+
+
+});
