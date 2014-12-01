c9debb3e7009e146510536836a0ca9632751bf4d
psieniarski <paabson@gmail.com>
2014-12-01 17:51:14 +0100

    Automated commit

--- src/settings.js
+++ src/settings.js
@@ -1,10 +1,12 @@
 module.exports = {
 	urls: {
-		api: 'https://ssl.filmweb.pl/api?'	
+		data: 'https://ssl.filmweb.pl/api?',
+		search: 'http:///www.filmweb.pl/search/live',	
 	},
 		
 	apiKey: 	'qjcGhW2JnvGT9dfCt3uT_jozR3s', 
 	appId: 		'android', 
 	version: 	'1.0',
+	log: 		console.log.apply(console), 
 
 }
