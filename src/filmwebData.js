5b2fc90c357efb09c8d19a574f41a9b399e020c5
psieniarski <paabson@gmail.com>
2014-12-01 19:07:39 +0100

    Automated commit

--- src/filmwebData.js
+++ src/filmwebData.js
@@ -15,7 +15,8 @@ var filmwebData = {
 
 		xhr.onreadystatechange = function() {
 		    if (xhr.readyState == 4) {
-		    	status.allways(xhr.responseText)
+		    	status.allways(xhr.responseText);
+		    	console.log('oko')
 		    	if (xhr.status == 200) {
 		    		status.success(xhr.responseText);
 
