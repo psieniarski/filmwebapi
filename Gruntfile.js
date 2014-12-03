9ceda9fdcd798a9daef6aa64a1e2b2b5c4d9f8d2
psieniarski <paabson@gmail.com>
2014-12-04 00:30:11 +0100

    Automated commit

--- Gruntfile.js
+++ Gruntfile.js
@@ -39,7 +39,7 @@ module.exports = function(grunt) {
                         expand: true,
                         overwrite: true,
                         cwd: 'src/',
-                        src: ['*'],
+                        src: ['*.js'],
                         dest: 'node_modules/',
                         filter: 'isFile',
                     },
