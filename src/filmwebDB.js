4023494089b5cac3ffcaa62e8d4e7c90dd9a8a9e
psieniarski <paabson@gmail.com>
2014-12-03 21:26:56 +0100

    Automated commit

--- src/filmwebDB.js
+++ src/filmwebDB.js
@@ -5,7 +5,7 @@ var XMLHttpRequest = require('xhr2');
 
 var filmwebDB = {
 
-	ajax: function( type, queryString, success, error ) {
+	ajax: function( type, data, success, error ) {
 
 		var xhr = new XMLHttpRequest();
 		
@@ -23,23 +23,18 @@ var filmwebDB = {
 		};
 
 		if ( type == 'search' || type == 'data' ) {
-		
-			xhr.open( 'GET', settings.urls[type] + queryString, true ); 
+			
+			xhr.open( 'GET', settings.urls[type] + convert.obj2url(data), true ); 
 			xhr.send();	
 		}
 	},
 
-	request: function(type, obj, success, error) {
-		var queryString = convert.obj2url(obj); 
-		this.ajax( type, queryString, success, error );
-	},
-
 	search: function( obj, success, error ) {
-		this.request('search', obj, success, error); 
+		this.ajax('search', obj, success, error); 
 	},
 
 	getData: function( obj, success, error ) {
-		this.request('data', obj, success, error); 
+		this.ajax('data', obj, success, error); 
 	}
 };
 
