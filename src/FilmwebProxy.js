ed31256a9e16281f0e4bd68090a2378e36603868
psieniarski <paabson@gmail.com>
2014-12-04 01:02:45 +0100

    Automated commit

--- src/FilmwebProxy.js
+++ src/FilmwebProxy.js
@@ -19,44 +19,44 @@ module.exports = {
 
 
 
-Request.prototype.getList = function() {
+// Request.prototype.getList = function() {
 
 
 
 
-};
+// };
 
-Request.prototype.prepareMethods = function(obj) {
-	var methods = [];
+// Request.prototype.prepareMethods = function(obj) {
+// 	var methods = [];
 
-	for (prop in obj) {
-		if (obj.hasOwnProperty(prop)) {
-			if (prop == 'getFilmInfoFull') {
-				for (var i = obj[prop].length - 1; i >= 0; i--) { 
-					methods.push(prop + ' ' + brackets( obj[prop][i] ) + '\\n');
-				};
-			} else {
-				methods.push(prop + ' ' + JSON.stringify(obj[prop]) + '\\n');			
-			}
-		}
-	}
-	return methods.join('');
-}
-
-
-Request.prototype.createSigniature = function(method) {
-	var hash = md5(method + settings.appId + settings.apiKey);
-	return settings.version + ',' + hash;
-}
+// 	for (prop in obj) {
+// 		if (obj.hasOwnProperty(prop)) {
+// 			if (prop == 'getFilmInfoFull') {
+// 				for (var i = obj[prop].length - 1; i >= 0; i--) { 
+// 					methods.push(prop + ' ' + brackets( obj[prop][i] ) + '\\n');
+// 				};
+// 			} else {
+// 				methods.push(prop + ' ' + JSON.stringify(obj[prop]) + '\\n');			
+// 			}
+// 		}
+// 	}
+// 	return methods.join('');
+// }
 
 
-// Zamiena obiekt na zakodowany łańcuch znaków URI
-var serialize = function(obj) {
-	var arr = []; 
-	for (prop in obj) {
-		if (obj.hasOwnProperty(prop)) {
-			arr.push(encodeURIComponent(prop) + '=' + encodeURIComponent(obj[prop]));
-		}
-	}
-	return arr.join('&');
-}
+// Request.prototype.createSigniature = function(method) {
+// 	var hash = md5(method + settings.appId + settings.apiKey);
+// 	return settings.version + ',' + hash;
+// }
+
+
+// // Zamiena obiekt na zakodowany łańcuch znaków URI
+// var serialize = function(obj) {
+// 	var arr = []; 
+// 	for (prop in obj) {
+// 		if (obj.hasOwnProperty(prop)) {
+// 			arr.push(encodeURIComponent(prop) + '=' + encodeURIComponent(obj[prop]));
+// 		}
+// 	}
+// 	return arr.join('&');
+// }
