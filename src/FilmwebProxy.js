626f09bfe81048e78a5ffd943514439c1e3fda33
psieniarski <paabson@gmail.com>
2014-12-04 03:31:59 +0100

    Automated commit

--- src/FilmwebProxy.js
+++ src/FilmwebProxy.js
@@ -2,9 +2,9 @@ var settings  = require('settings');
 var format	  = require('filmwebFormat');
 
 var filmwebDB = require('filmwebDB');
-'use strict';
-module.exports = {
 
+module.exports = {
+	
 	Request: function() {
 		var obj = {};
 
