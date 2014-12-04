6781449dc35dced4f8abbc6d016e670cb7e9cd8a
psieniarski <paabson@gmail.com>
2014-12-04 03:00:36 +0100

    Automated commit

--- src/filmwebDB.js
+++ src/filmwebDB.js
@@ -30,32 +30,8 @@ module.exports = {
 
 	search: function( obj, callback ) {
 		this.ajax( 'search', obj, function(err, success) { 
-
-			var films = success.responseText.split('\\a');
-			var arr = [];
-			for (var i = films.length - 1; i >= 0; i--) {
-				var part = (films[i].split('\\c'));
-				if (part.shift() == 'f') {
-					arr.push(part);
-				}
-			}
-
-			var newObj = {};
-			newObj.items = [];  
-			 
-
-			for (var i = 0; i < arr.length; i++) {
-				var film = arr[i];
-				var item = {
-					id: 		film[0],
-					thumbnails: film[1],
-					title:      film[2],
-					caption:    film[3],
-					caption2: 	film[4],
-					year:       film[5] 
-				}
-				newObj.items.push(item);
-			};
+			
+			
 
 			callback(null, newObj);
 		}); 
