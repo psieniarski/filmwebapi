4fd66d837f82c50db28b8e2bb26ad7287471aae6
psieniarski <paabson@gmail.com>
2014-12-04 03:01:39 +0100

    Automated commit

--- src/filmwebConvert.js
+++ src/filmwebConvert.js
@@ -7,5 +7,35 @@ module.exports = {
 			}
 		}
 		return arr.join('&');
+	},
+
+	search2obj: function() {
+
+		var films = success.responseText.split('\\a');
+			var arr = [];
+			for (var i = films.length - 1; i >= 0; i--) {
+				var part = (films[i].split('\\c'));
+				if (part.shift() == 'f') {  // jesli wynik jest filmem
+					arr.push(part);
+				}
+			}
+
+			var newObj = {};
+			newObj.items = [];  
+			 
+
+			for (var i = 0; i < arr.length; i++) {
+				var film = arr[i];
+				var item = {
+					id: 		film[0],
+					thumbnails: film[1],
+					title:      film[2],
+					caption:    film[3],
+					caption2: 	film[4],
+					year:       film[5] 
+				}
+				newObj.items.push(item);
+			};
+		}
 	}
 };
