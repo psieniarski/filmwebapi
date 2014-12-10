39e45afce9ee4e6245587129fe7ace3ddf199534
psieniarski <paabson@gmail.com>
2014-12-10 18:37:59 +0100

    Automated commit

--- src/filmwebDB.js
+++ src/filmwebDB.js
@@ -68,7 +68,7 @@ FilmwebDB.prototype = {
 		xhr.onreadystatechange = function() {
 		    if ( xhr.readyState == 4 ) {
 		    	if ( xhr.status == 200 ) {
-		    		console.log( new Date( xhr.getResponseHeader( 'date' ) ) );
+		    		console.log( new Date( xhr.getResponseHeader( 'date' ) ).getHours() );
 		    		that.emit('xhr', type, xhr);
 		    	} else {
 		    		that.emit( 'error', new Error( 'Error: ' + xhr.status ) );			
