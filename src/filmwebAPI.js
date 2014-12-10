177cd6301d3aacf61548067de5eb573687e05af0
psieniarski <paabson@gmail.com>
2014-12-10 17:09:10 +0100

    Automated commit

--- src/filmwebAPI.js
+++ src/filmwebAPI.js
@@ -1,53 +1,10 @@
+
 var filmwebAPI = function(source) {
 	this.source = source;
 };
 
 filmwebAPI.prototype = {
-	movies: {
 
-		/*	
-
-		input: 
-		{
-			q:  '',
-			id: '', 
-		}
-
-		output: 
-		{
-			items: [{
-				id: '',
-				token: '',
-				snippet: {
-					title: 		  '',
-					caption: 	  '',
-					url:          '',
-					thumbnails:   '',
-					description:  '',
-					vote:         0,
-					votesCount:   0, 
-					genre:        '', 
-					year:         0,
-					countries:    [],
-				}
-			}]
-		}
-			
-		*/
-		_Request: function(	type, data	) {
-			this.type = type; 
-			this.data = data;
-		},
-
-		list: function( obj ) {
-			return {
-				var request = new this._Request( obj );
-				execute: function( response ) {
-				 	filmwebProxy.add( request, response );
-				}
-			}
-		},
-	}
 }
 
 
