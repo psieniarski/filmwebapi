26598d72e082cb36ce0955ab9b23df1c098bc787
psieniarski <paabson@gmail.com>
2014-12-10 22:58:01 +0100

    Automated commit

--- src/filmweb.js
+++ src/filmweb.js
@@ -6,7 +6,7 @@ var fdb  = new FilmwebDB( fapi );
 
 for (var i = 1 - 1; i >= 0; i--) {
 	fapi.movies.list({ 
-		id: [122]
+		id: [122, 133]
 	}).execute(function( response ) {
 		console.log( response );
 	});
