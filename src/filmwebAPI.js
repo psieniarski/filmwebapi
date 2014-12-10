c8309a6224c78ea5df496e5812788ec22c6aa63f
psieniarski <paabson@gmail.com>
2014-12-10 22:00:06 +0100

    Automated commit

--- src/filmwebAPI.js
+++ src/filmwebAPI.js
@@ -16,6 +16,18 @@ var FilmwebAPI = function() {
 		that.emit( 'request', type, obj, timestamp );
 	};
 
+	var guid = (function() {
+		function s4() {
+		return  Math.floor((1 + Math.random()) * 0x10000)
+		            .toString(16)
+		            .substring(1);
+		}
+		return function() {
+		return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+		       s4() + '-' + s4() + s4() + s4();
+		};
+	})();
+
 	this.movies = {
 		list: function( obj ) {
 			return {
