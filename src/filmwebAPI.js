cbd235779772ffcede980f524eb24066c30fb054
psieniarski <paabson@gmail.com>
2014-12-10 17:06:52 +0100

    Automated commit

--- src/filmwebAPI.js
+++ src/filmwebAPI.js
@@ -1,57 +1,51 @@
-var filmwebProxy = require('filmwebProxy');
+var filmwebAPI = function(source) {
+	this.source = source;
+};
 
-module.exports = {
+var movies: {
 
-	movies: {
+	/*	
 
-		/*	
-
-		input: 
-		{
-			q:  '',
-			id: '', 
-		}
-
-		output: 
-		{
-			items: [{
-				id: '',
-				token: '',
-				snippet: {
-					title: 		  '',
-					caption: 	  '',
-					url:          '',
-					thumbnails:   '',
-					description:  '',
-					vote:         0,
-					votesCount:   0, 
-					genre:        '', 
-					year:         0,
-					countries:    [],
-				}
-			}]
-		}
-			
-		*/
-		_Request: function(	type, data	) {
-			this.type = type; 
-			this.data = data;
-		},
-
-		list: function( obj ) {
-			return {
-				var request = new this._Request( obj );
-				execute: function( response ) {
-				 	filmwebProxy.add( request, response );
-				}
-			}
-		},
+	input: 
+	{
+		q:  '',
+		id: '', 
 	}
+
+	output: 
+	{
+		items: [{
+			id: '',
+			token: '',
+			snippet: {
+				title: 		  '',
+				caption: 	  '',
+				url:          '',
+				thumbnails:   '',
+				description:  '',
+				vote:         0,
+				votesCount:   0, 
+				genre:        '', 
+				year:         0,
+				countries:    [],
+			}
+		}]
+	}
+		
+	*/
+	_Request: function(	type, data	) {
+		this.type = type; 
+		this.data = data;
+	},
+
+	list: function( obj ) {
+		return {
+			var request = new this._Request( obj );
+			execute: function( response ) {
+			 	filmwebProxy.add( request, response );
+			}
+		}
+	},
 }
-
-
-module.exports.movies.list({q: 'oko'}).execute(function(err, response) {
-	console.log(err, response);
-});
 
 
