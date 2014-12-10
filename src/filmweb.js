27880e876932fbd0b52a38bdec77869cbf8adf7e
psieniarski <paabson@gmail.com>
2014-12-10 17:29:29 +0100

    Automated commit

--- src/filmweb.js
+++ src/filmweb.js
@@ -4,5 +4,9 @@ var FilmwebAPI = require('filmwebAPI');
 var fapi = new FilmwebAPI(); 
 var fdb  = new FilmwebDB( fapi );
 
-fapi.movies.list
+fapi.movies.list({ 
+	q: 'oko'
+}).execute(function( response ) {
+	console.log( response )
+})
 
