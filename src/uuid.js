70feede3b9912336a045d81a2292eec6616041ee
psieniarski <paabson@gmail.com>
2014-12-10 22:49:45 +0100

    Automated commit

---
+++ src/uuid.js
@@ -0,0 +1,15 @@
+module.exports = {
+	uuid: (function() {
+		function s4() {
+		return  Math.floor((1 + Math.random()) * 0x10000)
+		            .toString(16)
+		            .substring(1);
+		}
+		return function() {
+		return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+		       s4() + '-' + s4() + s4() + s4();
+		};
+	})();
+}
+
+	
