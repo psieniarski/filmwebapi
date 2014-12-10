ab32a9ad7270b8e377cb9cecab66c08db7330352
psieniarski <paabson@gmail.com>
2014-12-10 20:34:53 +0100

    Automated commit

--- src/filmwebAPI.js
+++ src/filmwebAPI.js
@@ -17,8 +17,7 @@ var FilmwebAPI = function() {
 
 					that.emit( 'request', 'search', obj, timestamp );
 					callbacks[timestamp] = function( response ) {
-						console.log( response )
-						for ( prop in response ) {
+						for ( var prop in response ) {
 							if ( obj.hasOwnProperty( prop ) ) {
 								if ( response[prop] == 'id' ) {
 									console.log( response[prop] ); 
