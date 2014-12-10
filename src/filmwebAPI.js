efd9bc0f302a74522f720358646d8625d347933f
psieniarski <paabson@gmail.com>
2014-12-10 22:05:17 +0100

    Automated commit

--- src/filmwebAPI.js
+++ src/filmwebAPI.js
@@ -7,7 +7,7 @@ var FilmwebAPI = function() {
 	that.on( 'response', function( response, timestamp ) {
 		var callback = callbacks[timestamp];
 		callback( response );
-		console.log(callbacks);
+		delete callbacks[timestamp]
 	}); 
 
 	this._uuid = (function() {
