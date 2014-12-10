b6a98fc6efa135bf1ccf17e88e8f66be58f30934
psieniarski <paabson@gmail.com>
2014-12-10 22:52:05 +0100

    Automated commit

--- src/identification.js
+++ src/identification.js
@@ -1,15 +1,14 @@
-module.exports = {
-	uuid: (function() {
-		function s4() {
-		return  Math.floor((1 + Math.random()) * 0x10000)
-		            .toString(16)
-		            .substring(1);
-		}
-		return function() {
-		return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
-		       s4() + '-' + s4() + s4() + s4();
-		};
-	})();
-}
+module.exports = (function() {
+	function s4() {
+	return  Math.floor((1 + Math.random()) * 0x10000)
+	            .toString(16)
+	            .substring(1);
+	}
+	return function() {
+	return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+	       s4() + '-' + s4() + s4() + s4();
+	};
+})();
+
 
 	
