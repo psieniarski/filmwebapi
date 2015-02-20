ad8523b87dd5d38e0a261eed451c3be2bc39bed6
psieniarski <paabson@gmail.com>
2015-02-20 21:31:15 +0100

    formating

--- src/filmweb.js
+++ src/filmweb.js
@@ -5,20 +5,20 @@ var fapi = new FilmwebAPI();
 var fdb  = new FilmwebDB( fapi );
 
 module.exports = {
-	FilmwebDB: FilmwebDB,
-	FilmwebAPI: FilmwebAPI,
+  FilmwebDB: FilmwebDB,
+  FilmwebAPI: FilmwebAPI,
 
-	search: function(q, callback) {
-		fapi.movies.list({ 
-			q: q
-		}).execute(function( response ) {
-			callback( response );
-		});
-	}
+  search: function(q, callback) {
+    fapi.movies.list({ 
+      q: q
+    }).execute(function( response ) {
+      callback( response );
+    });
+  }
 };
 
 // module.exports.search('oko', function(response) {
-// 	console.log(response);
+//  console.log(response);
 // });
 
 
--- src/filmwebAPI.js
+++ src/filmwebAPI.js
@@ -3,42 +3,42 @@ var uuid       = require( './uuid.js' );
 
 
 var FilmwebAPI = function() {
-	var that 	  = this;
-	var callbacks = {};
+  var that    = this;
+  var callbacks = {};
 
-	this.on( 'response', function( response, id ) {
-		var callback = callbacks[id];
-		callback( response );
-		delete callbacks[id];
-	}); 
+  this.on( 'response', function( response, id ) {
+    var callback = callbacks[id];
+    callback( response );
+    delete callbacks[id];
+  }); 
 
-	this._request = function( type, obj, callback ) {
-		var id = uuid();
+  this._request = function( type, obj, callback ) {
+    var id = uuid();
 
-		callbacks[id] = callback;
-		that.emit( 'request', type, obj, id );
-	};
+    callbacks[id] = callback;
+    that.emit( 'request', type, obj, id );
+  };
 
-	this.movies = {
-		list: function( obj ) {
-			return {
-				execute: function( callback ) {
-					that._request( 'search', obj, function( response ) {
-						var ids   = [];
-						var items = response.items;
+  this.movies = {
+    list: function( obj ) {
+      return {
+        execute: function( callback ) {
+          that._request( 'search', obj, function( response ) {
+            var ids   = [];
+            var items = response.items;
 
-						for ( var i = 0; i < items.length; i++ ) {
-							ids.push( items[i].id );
-						};
+            for ( var i = 0; i < items.length; i++ ) {
+              ids.push( items[i].id );
+            };
 
-						that._request( 'data', { id: ids }, function( data ) {
-							callback( data );
-						});
-					});
-				}
-			};
-		},
-	};
+            that._request( 'data', { id: ids }, function( data ) {
+              callback( data );
+            });
+          });
+        }
+      };
+    },
+  };
 };
 
 emitter( FilmwebAPI.prototype );
@@ -48,46 +48,46 @@ module.exports = FilmwebAPI;
 
 // movies: {
 
-		
+    
 
-// 	input: 
-// 	{
-// 		q:  '',
-// 		id: '', 
-// 	}
+//  input: 
+//  {
+//    q:  '',
+//    id: '', 
+//  }
 
-// 	output: 
-// 	{
-// 		items: [{
-// 			id: '',
-// 			token: '',
-// 			snippet: {
-// 				title: 		  '',
-// 				caption: 	  '',
-// 				url:          '',
-// 				thumbnails:   '',
-// 				description:  '',
-// 				vote:         0,
-// 				votesCount:   0, 
-// 				genre:        '', 
-// 				year:         0,
-// 				countries:    [],
-// 			}
-// 		}]
-// 	}
-		
-	
-// 	_Request: function(	type, data	) {
-// 		this.type = type; 
-// 		this.data = data;
-// 	},
+//  output: 
+//  {
+//    items: [{
+//      id: '',
+//      token: '',
+//      snippet: {
+//        title:      '',
+//        caption:    '',
+//        url:          '',
+//        thumbnails:   '',
+//        description:  '',
+//        vote:         0,
+//        votesCount:   0, 
+//        genre:        '', 
+//        year:         0,
+//        countries:    [],
+//      }
+//    }]
+//  }
+    
+  
+//  _Request: function( type, data  ) {
+//    this.type = type; 
+//    this.data = data;
+//  },
 
-// 	list: function( obj ) {
-// 		return {
-// 			var request = new this._Request( obj );
-// 			execute: function( response ) {
-// 			 	filmwebProxy.add( request, response );
-// 			}
-// 		}
-// 	},
+//  list: function( obj ) {
+//    return {
+//      var request = new this._Request( obj );
+//      execute: function( response ) {
+//        filmwebProxy.add( request, response );
+//      }
+//    }
+//  },
 // }
